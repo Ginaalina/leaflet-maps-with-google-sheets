77dcf5ac0a04507540537e822027decf81c66fa9
Ginaalina <30727553+Ginaalina@users.noreply.github.com>
2017-08-22 16:19:29 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/15H2kWMcuusOO_xLPFKsmGiUdqDzpcGoQCiVv40MVpEs/edit#gid=1128321114';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/15H2kWMcuusOO_xLPFKsmGiUdqDzpcGoQCiVv40MVpEs/edit#gid=1913870153';
